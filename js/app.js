6eecf4371ea90385cb462469aa14daa32d63078a
Padmanaban Mohan <66869516+sri0711@users.noreply.github.com>
2021-11-17 13:52:07 +0530

    initial upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#navmain").fadeOut();
+  $(window).scroll(() => {
+    var scroll = $(window).scrollTop();
+    var objectSelect = $("#services");
+    var objectPosition = objectSelect.offset().top;
+
+    if (scroll > objectPosition - 100) {
+      $("#navmain").fadeIn();
+    } else {
+      $("#navmain").fadeOut();
+    }
+  });
+});
+
+var nav = document.getElementById("navbar");
+var navlink = document.getElementsByClassName("nav-link");
+
+for (var i = 0; i < navlink.length; i++) {
+  navlink[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active1");
+    current[0].className = current[0].className.replace(" active1", "");
+    this.className += " active1";
+  });
+}
