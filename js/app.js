68ff2d7c3126d2a0515963c8adb11322b77aacc2
sri0711 <m.sathish0711@gmail.com>
2021-11-17 17:01:33 +0530

    scroll progress bar

--- js/app.js
+++ js/app.js
@@ -23,3 +23,13 @@ for (var i = 0; i < navlink.length; i++) {
     this.className += " active1";
   });
 }
+
+const scrollProgress = document.getElementById('progress');
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
